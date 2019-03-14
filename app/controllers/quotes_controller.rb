418d5624ac5016f32acb739187870b6955ab19fc
Art <artelo.studio@gmail.com>
2019-03-14 21:25:32 +0000

    added logo footer link lesson12

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,5 +1,9 @@
 class QuotesController < ApplicationController
-def index
-  @quote = Quote.order("RANDOM()").first
-end
+  def index
+    @quote = Quote.order("RANDOM()").first
+  end
+
+  def new
+    @quote = Quote.new
+  end
 end
